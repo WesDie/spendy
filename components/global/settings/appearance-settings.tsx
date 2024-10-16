f544b955b6e0a7089d2a2e58cced44cc2e1bc6a4
WesDie <wes.dieleman@gmail.com>
2024-10-16 23:32:24 +0200

    refactor: optimsied appearance settings page

--- components/global/settings/appearance-settings.tsx
+++ components/global/settings/appearance-settings.tsx
@@ -57,73 +57,66 @@ export default function AppearanceSettings() {
   const { themeColor, setThemeColor } = useThemeContext();
 
   return (
-    <SettingsCard title="Appearance" description="Change your appearance.">
-      <Separator />
-      <div className="flex flex-col gap-2 mb-6">
-        <div className="flex flex-col gap-1">
+    <SettingsCard
+      title="Appearance"
+      description="Change your appearance."
+      label="These settings are applied globally and are saved locally."
+    >
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-2">
           <h4 className="text-md font-semibold">Color scheme</h4>
           <p className="text-sm text-muted-foreground">
             Change the color scheme of the platform.
           </p>
+          <Select defaultValue={theme} onValueChange={setTheme}>
+            <SelectTrigger className="w-[200px]">
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="dark">Dark</SelectItem>
+              <SelectItem value="light">Light</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
-        <Select defaultValue={theme} onValueChange={setTheme}>
-          <SelectTrigger className="w-[200px]">
-            <SelectValue />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="dark">Dark</SelectItem>
-            <SelectItem value="light">Light</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-      <div className="flex flex-col gap-2">
-        <div className="flex flex-col gap-1">
+
+        <div className="flex flex-col gap-2">
           <h4 className="text-md font-semibold">Theme</h4>
           <p className="text-sm text-muted-foreground">
             Change the theme of the platform.
           </p>
-        </div>
-        <RadioGroup
-          defaultValue={themeColor}
-          onValueChange={(value) => setThemeColor(value as ThemeColors)}
-          className="flex flex-wrap gap-4 w-fit"
-        >
-          {colorThemes.map(({ name, light, dark }) => (
-            <Label
-              key={name}
-              className={`flex flex-col items-center space-y-2 rounded-md cursor-pointer group`}
-            >
-              <RadioGroupItem value={name} id={name} className="sr-only" />
-              <div
-                className={`p-1 rounded-lg group-hover:bg-muted-foreground/40 transition-colors duration-300 ${
-                  themeColor === name
-                    ? `ring-2 ${
-                        theme === "light" ? "ring-black" : "ring-white"
-                      }`
-                    : ""
-                }`}
+          <RadioGroup
+            defaultValue={themeColor}
+            onValueChange={(value) => setThemeColor(value as ThemeColors)}
+            className="flex flex-wrap gap-4 w-fit"
+          >
+            {colorThemes.map(({ name, light, dark }) => (
+              <Label
+                key={name}
+                className="flex flex-col items-center space-y-2 rounded-md cursor-pointer group"
               >
+                <RadioGroupItem value={name} id={name} className="sr-only" />
                 <div
-                  className={`w-[150px] h-[75px] p-2 rounded-md ${
-                    theme === "light" ? light : dark
+                  className={`p-1 rounded-lg group-hover:bg-muted-foreground/20 transition-colors duration-300 ${
+                    themeColor === name
+                      ? `ring-2 ${
+                          theme === "light" ? "ring-black" : "ring-white"
+                        }`
+                      : ""
                   }`}
-                />
-              </div>
-              <span className="text-sm font-medium">
-                {name.charAt(0).toUpperCase() + name.slice(1)}
-              </span>
-            </Label>
-          ))}
-        </RadioGroup>
-      </div>
-      <Separator />
-      <div className="flex justify-between gap-2 h-9">
-        <Label
-          htmlFor="theme"
-          className="text-muted-foreground my-auto font-normal"
-        >
-          These settings are applied globally and are saved locally.
-        </Label>
+                >
+                  <div
+                    className={`w-[150px] h-[75px] p-2 rounded-md ${
+                      theme === "light" ? light : dark
+                    }`}
+                  />
+                </div>
+                <span className="text-sm font-medium">
+                  {name.charAt(0).toUpperCase() + name.slice(1)}
+                </span>
+              </Label>
+            ))}
+          </RadioGroup>
+        </div>
       </div>
     </SettingsCard>
   );
